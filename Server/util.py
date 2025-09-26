2d79021954dd8eeafe0f74c4cd42b03637998fc6
Aboobacker Sidheeque <sidhub41214@gmail.com>
2025-09-26 11:13:36 +0530

    server

---
+++ Server/util.py
@@ -0,0 +1,29 @@
+import json
+import pickle
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_location_names():
+    global __locations
+    return __locations
+
+def load_saved_artifacts():
+    print("loading saved artifacts...--> START ")
+    global __data_columns
+    global __locations
+    
+    with open('./artifacts/columns.json','r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+        
+    with open('./artifacts/bangalore_home_prices_model.pickle','rb') as f:
+        __model = pickle.load(f)
+    print("Loading Saved Artifacts....-->DONE")    
+        
+        
+        
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
